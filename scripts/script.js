ee889cecfea42f3dc09a17ff997867285487d710
Vol unteers <vol8@qu-a10.nov.21-school.ru>
2023-10-01 15:52:13 +0300

    fix bug footer and error handling

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const accessKey = 'YiyymxUO5Lagn4m1wPZVObT6TJnZi3a0YRnxy9uX5d8'; 
-const photoContainer = document.getElementById('photoContainer');
-const searchInput = document.getElementById('searchInput');
+const accessKey = "YiyymxUO5Lagn4m1wPZVObT6TJnZi3a0YRnxy9uX5d8";
+const photoContainer = document.getElementById("photoContainer");
+const searchInput = document.getElementById("searchInput");
 
 async function fetchPhotos(query) {
   const response = await fetch(`https://api.unsplash.com/photos/random?query=${query}&client_id=${accessKey}&count=10`);
@@ -9,15 +9,27 @@ async function fetchPhotos(query) {
 }
 
 function displayPhotos(photos) {
-  photoContainer.innerHTML = '';
-  photos.forEach(photo => {
-    const img = document.createElement('img');
-    img.src = photo.urls.regular;
-    img.alt = photo.alt_description || 'Photo';
-    photoContainer.appendChild(img);
-  });
+  const photoContainer = document.getElementById("photoContainer");
+  if (Array.isArray(photos) && photos.length > 0) {
+    photoContainer.innerHTML = "";
+    photos.forEach((photo) => {
+      const img = document.createElement("img");
+      img.src = photo.urls.regular;
+      img.alt = photo.alt_description || "Photo";
+      photoContainer.appendChild(img);
+    });
+    if (photos.length > 4) {
+      const footer = document.querySelector("footer");
+      footer.style.position = "relative";
+    }
+  } else {
+    Swal.fire({
+      title: "Please enter a correct input",
+      icon: "error",
+      confirmButtonText: "OK",
+    });
+  }
 }
-
 async function searchPhotos() {
   const query = searchInput.value;
   if (query) {
@@ -25,13 +37,28 @@ async function searchPhotos() {
       const photos = await fetchPhotos(query);
       displayPhotos(photos);
     } catch (error) {
-      console.error('Error fetching photos:', error);
+      if (error instanceof SyntaxError && error.message.includes("Rate Limit Exceeded")) {
+        Swal.fire({
+          title: "API Rate Limit Exceeded",
+          text: "Please try again later.",
+          icon: "error",
+          confirmButtonText: "OK",
+        });
+      } else {
+        console.error("Error fetching photos:", error);
+      }
     }
   } else {
     Swal.fire({
-        title: "Please enter a search term",
-        icon: "error",
-        confirmButtonText: "OK",
-      });
+      title: "Please enter a search term",
+      icon: "error",
+      confirmButtonText: "OK",
+    });
+  }
+}
+
+function handleKeyPress(event) {
+  if (event.key === "Enter") {
+    searchPhotos();
   }
 }
